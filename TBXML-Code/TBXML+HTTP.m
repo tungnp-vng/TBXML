1df503ed088e49c21747ff00dca778a64905c700
Tung Nguyen <tungnp@vng.com.vn>
2015-12-30 17:37:04 +0700

    Update Project Setting

--- TBXML-Code/TBXML+HTTP.m
+++ TBXML-Code/TBXML+HTTP.m
@@ -30,7 +30,7 @@
 	}
 	
 	NSData * postData = [[params componentsJoinedByString:@"&"] dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-	NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+	NSString *postLength = [NSString stringWithFormat:@"%d", (int)[postData length]];
 	
 	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
 	[request setURL:url];
